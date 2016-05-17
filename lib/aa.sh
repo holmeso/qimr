7526e11cce91d9e0e9022269f2e6d0567b144290
Christina.xu <christina.xu@qimrberghofer.edu.au>
2016-05-17 05:11:52 +0000

    delete accident code

--- lib/aa.sh
+++
@@ -1,7 +0,0 @@
-ln -s /Users/christix/Documents/Eclipse/sourceForge/trunk/adama/build/lib/qcommon-0.3.jar qcommon-0.3.jar
-ln -s /Users/christix/Documents/Eclipse/sourceForge/trunk/adama/build/lib/qlib-0.1pre.jar qlib-0.1pre.jar
-ln -s /Users/christix/Documents/Eclipse/sourceForge/trunk/adama/build/lib/qmaftools-0.2.jar qmaftools-0.2.jar
-ln -s /Users/christix/Documents/Eclipse/sourceForge/trunk/adama/build/lib/qbamfilter-1.2.jar qbamfilter-1.2.jar
-ln -s /Users/christix/Documents/Eclipse/sourceForge/trunk/adama/build/lib/qpicard-1.1.jar qpicard-1.1.jar
-ln -s /Users/christix/Documents/Eclipse/sourceForge/trunk/adama/build/lib/qio-0.1pre.jar qio-0.1pre.jar
-ln -s /Users/christix/Documents/Eclipse/sourceForge/trunk/adama/build/lib/qmule-0.1pre.jar qmule-0.1pre.jar
